b102d15788ef6787edbafbee04bcf6054aebf58d
Ana Villagran <avillagran@MBP-de-Ana.lan>
2019-07-25 21:01:40 -0400

    Add fail test

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -4,12 +4,12 @@ const coTest = require('../src/coTest');
 const CarInsurance = coTest.CarInsurance;
 const Product = coTest.Product;
 
-describe("Co Test", function() {
+describe('Car Covarage', () => {
 
-  it("should foo", function() {
-    const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
+  it('Check valid covarage', () => {
+    const coTest = new CarInsurance([ new Product('foo', 0, 0) ]);
     const products = coTest.updatePrice();
-    expect(products[0].name).equal("fixme");
+    expect(products).to.be.a('array');
   });
 
 });
