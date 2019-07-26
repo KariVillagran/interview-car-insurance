4b6553f37220655b943f9156fdd41773290894fe
Ana K. Villagran <akarinavillagrani@gmail.com>
2019-07-25 21:31:46 -0400

    add sellIn and price checks when covarage is normal

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -4,12 +4,35 @@ const coTest = require('../src/coTest');
 const CarInsurance = coTest.CarInsurance;
 const Product = coTest.Product;
 
-describe('Car Covarage', () => {
+describe('Car Normal Covarage', () => {
 
   it('Check valid covarage', () => {
-    const coTest = new CarInsurance([ new Product('foo', 0, 0) ]);
+    const nameCovarage = 'foo';
+    const sellInCovarage = 0;
+    const priceCovarage = 0;
+    const coTest = new CarInsurance([ new Product(nameCovarage, sellInCovarage, priceCovarage) ]);
     const products = coTest.updatePrice();
     expect(products).to.be.a('array');
+  });
+
+  it('Check price when sellIn is positive', () => {
+    const nameCovarage = 'foo';
+    const sellInCovarage = 4;
+    const priceCovarage = 5;
+    const coTest = new CarInsurance([ new Product(nameCovarage, sellInCovarage, priceCovarage) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).to.equal(4);
+    expect(products[0].sellIn).to.equal(3);
+  });
+
+  it('Check price when sellIn is negative', () => {
+    const nameCovarage = 'foo';
+    const sellInCovarage = -1;
+    const priceCovarage = 4;
+    const coTest = new CarInsurance([ new Product(nameCovarage, sellInCovarage, priceCovarage) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].price).to.equal(2);
+    expect(products[0].sellIn).to.equal(-2);
   });
 
 });
