c48b82c2a154e802ef22b34e2b6e00eef580e096
Ana K. Villagran <akarinavillagrani@gmail.com>
2019-07-25 21:43:17 -0400

    apply normal covarage with rules

--- src/coTest.js
+++ src/coTest.js
@@ -10,8 +10,23 @@ class CarInsurance {
   constructor(products = []) {
     this.products = products;
   }
+
+  updateNormalCovarage(product) {
+    let decreases = 1;
+    if (product.sellIn < 0) {
+      decreases = 2;
+    }
+    if(product.price >= 0) {
+      product.price = product.price - decreases;
+    } else {
+      product.price = 0;
+    }
+    product.sellIn = product.sellIn - 1;
+    return product;
+  }
+
   updatePrice() {
-    this.products = this.products.map(product => product);
+    this.products = this.products.map(product => this.updateNormalCovarage(product));
     return this.products;
   }
 }
